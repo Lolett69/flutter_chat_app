c9908a54bf693cf1b5fe13580ecde6b4f3cff5cc
Lolett69 <lolett69@gmail.com>
2025-05-19 21:39:19 +0800

    Organizing the styles and colors for the login page

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -1,4 +1,6 @@
 import 'package:chat_app/chat_page.dart';
+import 'package:chat_app/utils/textfield_styles.dart';
+import 'package:chat_app/widgets/login_text_field.dart';
 import 'package:flutter/material.dart';
 
 
@@ -77,7 +79,8 @@ final passwordController = TextEditingController();
               child: Column(
 
                 children: [
-                  TextFormField(
+                  LoginTextField(
+                    hintText: 'Enter your username',
                     validator: (value) {
                       if (value != null && value.isNotEmpty && value.length < 5) {
                         return 'Your username must be at least 5 characters long';
@@ -88,29 +91,17 @@ final passwordController = TextEditingController();
                     },
 
                     controller: userNameController,
-                    decoration: InputDecoration(
-                      hintText: 'Add your username',
-                      hintStyle: TextStyle(
-                        color: Colors.blueGrey,
-                      ),
-                      border: OutlineInputBorder(),
-                    ),
+                  
                   ),
 
                   SizedBox(
                     height: 24
                     ),
 
-                  TextFormField(
-                    controller: passwordController,
-                    obscureText: true,
-                    decoration: InputDecoration(
-                      hintText: 'Type your Password',
-                      hintStyle: TextStyle(
-                        color: Colors.blueGrey,
-                      ),
-                      border: OutlineInputBorder(),
-                    ),
+                  LoginTextField(
+                    hashAsterisk: true,
+                    controller: passwordController, hintText: 'Enter your password',
+                   
                   ),
                 ],
               ),
