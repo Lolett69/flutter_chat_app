f9284237d9492a427cf982db9a98787a0a29074f
Lolett69 <lolett69@gmail.com>
2025-05-19 08:58:59 +0800

    Writing your first stateful widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:chat_app/chat_page.dart';
+import 'package:chat_app/counter_stateful_demo.dart';
 import 'package:chat_app/login_page.dart';
 import 'package:flutter/material.dart';
 
@@ -14,7 +15,7 @@ class ChatApp extends StatelessWidget {
       title: "Chat App",
       theme: ThemeData(primarySwatch: Colors.yellow),
       //
-      home: ChatPage(),
+      home: const CounterStateful(),
     );
   }
 }
