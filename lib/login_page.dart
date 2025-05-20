d8dd3499a1384ae777d6381cfcacc47104237b8f
Lolett69 <lolett69@gmail.com>
2025-05-20 10:10:43 +0800

    Exploring pub.dev.

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -113,9 +113,13 @@ final passwordController = TextEditingController();
                 loginUser(context);
               },
 
+              style: ElevatedButton.styleFrom(
+              backgroundColor: Colors.deepPurple, // Set background color
+            ),
+
               child: Text(
                 'Login', 
-                style: TextStyle(fontSize: 24, fontWeight: FontWeight.w300),
+                style: TextStyle(fontSize: 24, color: Colors.white, fontWeight: FontWeight.w300),
               ),
             ),
 
--- lib/utils/textfield_styles.dart
+++ lib/utils/textfield_styles.dart
@@ -1,10 +1,10 @@
+import 'package:google_fonts/google_fonts.dart';
 import 'package:flutter/material.dart';
 
 
 class ThemeTextStyle{
-  static TextStyle loginTextFieldStyle = TextStyle(
-    color: Colors.blueGrey,
-    fontWeight: FontWeight.w500,
-    letterSpacing: 0.5,
+  static TextStyle loginTextFieldStyle = 
+  GoogleFonts.lato(textStyle: TextStyle(color: Colors.blueGrey),
+
   );
 }
